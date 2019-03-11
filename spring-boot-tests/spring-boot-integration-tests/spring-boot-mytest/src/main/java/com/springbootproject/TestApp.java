427c893d66a8de6b78c79f695d02f42ee0026d34
jiyifa <jiyifa2017@126.com>
2019-03-11 14:41:09 +0800

    System.out.println("这是测试项目");，输出提示

--- spring-boot-tests/spring-boot-integration-tests/spring-boot-mytest/src/main/java/com/springbootproject/TestApp.java
+++ spring-boot-tests/spring-boot-integration-tests/spring-boot-mytest/src/main/java/com/springbootproject/TestApp.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //TestApp
 @SpringBootApplication
 public class TestApp {
+
 	public static void main(String[] args) {
 		SpringApplication.run(TestApp.class, args);
+		System.out.println("这是测试项目");
 	}
+
 }
